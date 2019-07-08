3031eeb838dd32c6f657c8a2bdaa42e3d84c919e
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-08 21:07:05 +0700

    Скорректировал Age

--- src/Coursework/Age.java
+++ src/Coursework/Age.java
@@ -9,25 +9,24 @@ public class Age {
         System.out.println("Введите свой возраст от 1 года до 112 лет включительно:");
         int age = scanner.nextInt();
 
+        String ending = "";
+
         if (age < 1) {
             System.out.println("Вы слишком малы");
         } else if (age > 112) {
             System.out.println("Вы слишком стары");
         } else {
-            if ((age == 11) || (age == 12) || (age == 13) || (age == 14) || (age == 111) || (age == 112)) {
-                String ending = "лет";
-                System.out.printf("Вам %d %s", age, ending);
+            if ((age % 100 == 11) || (age % 100 == 12) || (age % 100 == 13) || (age % 100 == 14)) {
+                ending = "лет";
             } else if (age % 10 == 1) {
-                String ending = "год";
-                System.out.printf("Вам %d %s", age, ending);
+                ending = "год";
             } else if ((age % 10 == 2) || (age % 10 == 3) || (age % 10 == 4)) {
-                String ending = "года";
-                System.out.printf("Вам %d %s", age, ending);
+                ending = "года";
             } else {
-                String ending = "лет";
-                System.out.printf("Вам %d %s", age, ending);
+                ending = "лет";
             }
         }
+        System.out.printf("Вам %d %s", age, ending);
     }
 }
 
