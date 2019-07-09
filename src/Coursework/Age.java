725fe9928bcad3ec42a2b8b6b8eada936b648735
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-09 21:02:53 +0700

    Добавил класс TriangleArea

--- src/Coursework/Age.java
+++ src/Coursework/Age.java
@@ -9,24 +9,27 @@ public class Age {
         System.out.println("Введите свой возраст от 1 года до 112 лет включительно:");
         int age = scanner.nextInt();
 
-        String ending = "";
+        String ending;
+
+        int divisionRemainder100 = age % 100;
+        int divisionRemainder10 = age % 10;
 
         if (age < 1) {
             System.out.println("Вы слишком малы");
         } else if (age > 112) {
             System.out.println("Вы слишком стары");
         } else {
-            if ((age % 100 == 11) || (age % 100 == 12) || (age % 100 == 13) || (age % 100 == 14)) {
+            if ((divisionRemainder100 == 11) || (divisionRemainder100 == 12) || (divisionRemainder100 == 13) || (divisionRemainder100 == 14)) {
                 ending = "лет";
             } else if (age % 10 == 1) {
                 ending = "год";
-            } else if ((age % 10 == 2) || (age % 10 == 3) || (age % 10 == 4)) {
+            } else if ((divisionRemainder10 == 2) || (divisionRemainder10 == 3) || (divisionRemainder10 == 4)) {
                 ending = "года";
             } else {
                 ending = "лет";
             }
+            System.out.printf("Вам %d %s", age, ending);
         }
-        System.out.printf("Вам %d %s", age, ending);
     }
 }
 
