79e6a7df04aa3a23db24991e7b63671310b322d7
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-12 00:33:59 +0700

    Apartments

--- src/Coursework/Apartments.java
+++ src/Coursework/Apartments.java
@@ -6,7 +6,7 @@ public class Apartments {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int quantityApartmentOnFloor = 4;
+        int apartmentsOnFloorQuantity = 4;
 
         System.out.println("Введите количество этажей: ");
         int floorsQuantity = scanner.nextInt();
@@ -17,19 +17,18 @@ public class Apartments {
         System.out.println("Введите номер квартиры: ");
         int apartmentNumber = scanner.nextInt();
 
-        if (apartmentNumber > (floorsQuantity * porchesQuantity * quantityApartmentOnFloor)) {
+        if (apartmentNumber > (floorsQuantity * porchesQuantity * apartmentsOnFloorQuantity)) {
             System.out.println("Такой квартиры в этом доме нет");
-        } else if (floorsQuantity <= 0 || porchesQuantity <= 0 || apartmentNumber <= 0) {
+        } else if ((floorsQuantity <= 0) || (porchesQuantity <= 0) || (apartmentNumber <= 0)) {
             System.out.println("Введите корректные данные");
         } else {
-
-            int porchNumber =(int) Math.ceil((double) apartmentNumber / (floorsQuantity * quantityApartmentOnFloor));
+            int porchNumber = (int) Math.ceil((double) apartmentNumber / (floorsQuantity * apartmentsOnFloorQuantity));
             System.out.printf("Квартира находится в подъезде № %d, ", porchNumber);
 
-            int floorNumber = (((apartmentNumber - 1) % (quantityApartmentOnFloor * floorsQuantity)) / quantityApartmentOnFloor) + 1;
+            int floorNumber = (((apartmentNumber - 1) % (apartmentsOnFloorQuantity * floorsQuantity)) / apartmentsOnFloorQuantity) + 1;
             System.out.printf("на %d этаже, ", floorNumber);
 
-            int doorPosition = apartmentNumber % quantityApartmentOnFloor;
+            int doorPosition = apartmentNumber % apartmentsOnFloorQuantity;
             if (doorPosition == 1) {
                 System.out.println("ближняя слева.");
             } else if (doorPosition == 2) {
