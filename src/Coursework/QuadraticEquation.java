e8eb5fb86af796b9835988eec33c9c1b81b9e030
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-14 00:45:10 +0700

    QuadraticEquation

--- src/Coursework/QuadraticEquation.java
+++ src/Coursework/QuadraticEquation.java
@@ -14,33 +14,34 @@ public class QuadraticEquation {
         System.out.println("c = ");
         double c = scanner.nextDouble();
 
-        double discriminant = Math.pow(b, 2) - 4 * a * c;
+        if (a != 0) {
+            System.out.println("Это квадратное уровнение");
+            double discriminant = Math.pow(b, 2) - 4 * a * c;
 
-        if (a == 0 && b != 0 && c != 0) {
-            System.out.println("Это не квадратное уровнение, х = " + (-1 * c) / b);
-        } else if ((a != 0 && b == 0 && c != 0) && (-1 * c / a) > 0) {
-            double x1 = -1 * Math.sqrt(-1 * c / a);
-            double x2 = Math.sqrt(-1 * c / a);
-            System.out.printf("Корни квадратного уравнения: x1 = %.2f, x2 = %.2f", x1, x2);
-        } else if ((a != 0 && b == 0 && c != 0) && (-1 * c / a) < 0) {
-            System.out.println("Кореней квадратного уровнения нет");
-        } else if (a != 0 && b != 0 && c == 0) {
-            System.out.printf("Корни квадратного уравнения: x1 = 0, x2 = %.2f", (-1 * b / a));
-        } else if (a != 0 && b == 0 && c == 0) {
-            System.out.println("Корень квадратного уровнения х = 0");
-        } else if ((a == 0 && b != 0 && c == 0)) {
-            System.out.println("Корень квадратного уровнения х = 0");
-        } else if ((a == 0 && b == 0 && c == 0) || (a == 0 && b == 0 && c != 0)) {
-            System.out.println("Решения нет");
-        } else if (discriminant > 0) {
-            double x1 = (-1 * b - Math.sqrt(discriminant)) / (2 * a);
-            double x2 = (-1 * b + Math.sqrt(discriminant)) / (2 * a);
-            System.out.printf("Корни квадратного уравнения: x1 = %.2f, x2 = %.2f", x1, x2);
-        } else if (discriminant == 0) {
-            double x = -b / (2 * a);
-            System.out.printf("Корень квадратного уравнения x = %.2f", x);
+            if (discriminant > 0) {
+                double x1 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                double x2 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                System.out.printf("Корни квадратного уравнения: x1 = %.2f, x2 = %.2f", x1, x2);
+
+            } else if (discriminant == 0) {
+                double x = (-b + Math.sqrt(discriminant)) / (2 * a);
+                System.out.printf("Корень квадратного уравнения: x = %.2f", x);
+
+            } else {
+                System.out.print("Кореней квадратного уравнения нет");
+            }
+
+        } else if (b == 0 && c == 0) {
+            System.out.print("Это не квадратное уровнение, которое имеет бесконечное множество корней");
+
+        } else if (b != 0 && c == 0) {
+            System.out.print("Это не квадратное уровнение, корень x = 0");
+
+        } else if (b != 0 && c != 0) {
+            System.out.printf("Это не квадратное уровнение, корень x = %.2f", (-c / b));
+
         } else {
-            System.out.println("Кореней квадратного уровнения нет");
+            System.out.print("Это не квадратное уровнение, решения нет");
         }
     }
 }
