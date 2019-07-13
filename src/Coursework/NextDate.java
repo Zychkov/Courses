1ec5974a0da61e3ded503bbf5bd1cbb24ddc01a4
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-13 22:17:54 +0700

    Apartments

--- src/Coursework/NextDate.java
+++ src/Coursework/NextDate.java
@@ -17,9 +17,9 @@ public class NextDate {
         System.out.println("день:");
         int day = scanner.nextInt();
 
-        boolean isLeapYear = (year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0);
+        boolean isLeapYear = (year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0));
 
-        int maxDayInMonth;
+        int maxDayInMonth = 31;
 
         if ((month == 4) || (month == 6) || (month == 9) || (month == 11)) {
             maxDayInMonth = 30;
@@ -27,11 +27,9 @@ public class NextDate {
             maxDayInMonth = 29;
         } else if (!isLeapYear && month == 2) {
             maxDayInMonth = 28;
-        } else {
-            maxDayInMonth = 31;
         }
 
-        if (month < 1 || day < 1 || month > 12 || day > maxDayInMonth){
+        if (month < 1 || day < 1 || month > 12 || day > maxDayInMonth) {
             System.out.println("Такой даты нет");
         } else if (day == maxDayInMonth && month == 12) {
             int nextDay = 1;
@@ -44,8 +42,7 @@ public class NextDate {
             System.out.printf("Завтра будет %d.%d.%d г.", nextDay, nextMonth, year);
         } else {
             int nextDay = day + 1;
-            int nextMonth = month + 1;
-            System.out.printf("Завтра будет %d.%d.%d г.", nextDay, nextMonth, year);
+            System.out.printf("Завтра будет %d.%d.%d г.", nextDay, month, year);
         }
     }
 }
