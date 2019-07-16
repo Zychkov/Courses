9367bdb072425ffd210ff6de0db51edd85111d86
Zychkov <Zychkov.artem@gmail.com>
2019-07-16 21:53:18 +0700

    break

--- src/Lesson5/Break.java
+++ src/Lesson5/Break.java
@@ -1,4 +1,22 @@
 package Lesson5;
 
+import java.util.Scanner;
+
 public class Break {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String word = "break";
+
+        for ( ; ; ) {
+            System.out.println("Введите слово:");
+            String userWord = scanner.nextLine();
+
+            if (word.equals(userWord)) {
+                System.out.println("Вы угадали!!!");
+                break;
+            }
+            System.out.println("Попробуйте еще раз");
+        }
+    }
 }
