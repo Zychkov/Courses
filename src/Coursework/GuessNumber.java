d2c676c277743e11b0573b74306aa6ed1ac21fed
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-18 22:56:46 +0700

    Lesson5

---
+++ src/Coursework/GuessNumber.java
@@ -0,0 +1,25 @@
+package Coursework;
+
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Угадайте целое число от 0 до 100");
+
+        int unknownNumber = (int) Math.floor(Math.random() * 100);
+
+        for (int i = 1 ; ; i++) {
+            System.out.print("Введите ваше число: ");
+            int userNumber = scanner.nextInt();
+
+            if (userNumber == unknownNumber) {
+                System.out.printf("Вы угадали, количество попыток = %d. Поздравляю !!!", i);
+                break;
+            } else if (userNumber > unknownNumber) {
+                System.out.println("Ваше число слишком большое");
+            } else System.out.println("Ваше число слишком маленькое");
+        }
+    }
+}
