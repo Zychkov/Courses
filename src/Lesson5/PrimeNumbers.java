477093be846753f2f731ac13935547deadab8648
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-20 21:48:55 +0700

    Lesson5

--- src/Lesson5/PrimeNumbers.java
+++ src/Lesson5/PrimeNumbers.java
@@ -2,26 +2,26 @@ package Lesson5;
 
 import java.util.Scanner;
 
-public class SimpleNumbers {
+public class PrimeNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введите целое число n = ");
         int n = scanner.nextInt();
 
-        boolean isPrime = true;
-        int j;
-
         for (int i = 2; i <= n; i++) {
-            for (j = 2; j < i ; j++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j < i; j++) {
                 if (i % j == 0) {
                     isPrime = false;
                     break;
                 }
             }
+
             if (isPrime) {
-            System.out.println(j);
-            } else isPrime = true;
+                System.out.println(i);
+            }
         }
     }
 }
