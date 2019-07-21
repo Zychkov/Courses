a2679da4fd5a2feb9a7fc2d1dbba8622e53be85b
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-21 20:31:08 +0700

    Lesson6

---
+++ src/Lesson6/OrderCost.java
@@ -0,0 +1,39 @@
+package Lesson6;
+
+import java.util.Scanner;
+
+public class OrderCost {
+    private static int getDiscountProductsQuantity(double totalQuantity) {
+        return (totalQuantity >= 10) ? 5 : 0;
+    }
+
+    private static int getDiscountProductsCoast(double totalCoast) {
+        return (totalCoast >= 1000) ? 5 : 0;
+    }
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Введите количество товаров А");
+        int productAQuantity = s.nextInt();
+
+        System.out.println("Введите количество товаров Б");
+        int productBQuantity = s.nextInt();
+
+        double productAPrice = 200;
+        double productBPrice = 100;
+
+        int totalQuantity = productAQuantity + productBQuantity;
+
+        double totalCoast = productAPrice * productAQuantity + productBPrice * productBQuantity;
+
+        int totalDiscount = getDiscountProductsQuantity (totalQuantity) + getDiscountProductsCoast(totalCoast);
+
+        double totalCoastWithDiscount = totalCoast - totalCoast * totalDiscount / 100;
+
+        System.out.printf("Итоговая стоимость всех товаров составляет %.2f р. c с учетом скидки %d %%", totalCoastWithDiscount, totalDiscount);
+    }
+}
+
+
+
