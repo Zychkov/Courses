7e6f69cfcacc4b3da5001288b9345303cfe40148
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-07-23 21:59:48 +0700

    Исправления 23.07

--- src/Coursework/EuclideanAlgorithm.java
+++ src/Coursework/EuclideanAlgorithm.java
@@ -6,10 +6,10 @@ public class EuclideanAlgorithm {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Введите большее число:");
+        System.out.println("Введите первое число:");
         int a = scanner.nextInt();
 
-        System.out.println("Введите меньшее число:");
+        System.out.println("Введите второе число:");
         int b = scanner.nextInt();
 
         int nod = 0;
