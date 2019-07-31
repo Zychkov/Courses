44848e5202f40797a2b1153216492bbb58d6d710
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-08-01 00:02:36 +0700

    Lesson9

--- src/Lesson9/ArrayMultiplicationTable.java
+++ src/Lesson9/ArrayMultiplicationTable.java
@@ -5,17 +5,17 @@ package Lesson9;
 import java.util.Scanner;
 
 public class ArrayMultiplicationTable {
-    /*private static int getArray(int tableSize) {
+    private static int[][] getArray(int tableSize) {
         int[][] array = new int[tableSize][tableSize];
 
-        for (int i = 0, int j = 0; i<array[].lenght ; i++, j++){
-
-
+        for (int i = 0; i < array.length; ++i) {
+            for (int j = 0; j < array[0].length; ++j) {
+                array[i][j] = (i + 1) * (j + 1);
+            }
         }
 
-
-        return;
-    }*/
+        return array;
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -23,6 +23,17 @@ public class ArrayMultiplicationTable {
         System.out.println("Введите размер таблицы:");
         int tableSize = scanner.nextInt();
 
-        //getArray(tableSize);
+        int[][] array = getArray(tableSize);
+
+        //System.out.println(Arrays.deepToString(array) + "\n");
+
+        for (int[] e : array) {
+            for (int a : e) {
+                System.out.print(a + "\t");
+            }
+
+            System.out.println();
+        }
     }
 }
+
