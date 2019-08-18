cb489df1ef7b7bafcc0b1797e9724060d5e505a0
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-08-18 23:16:41 +0700

    Blurring

--- src/Lesson13/Blurring.java
+++ src/Lesson13/Blurring.java
@@ -27,28 +27,30 @@ public class Blurring {
         int[] pixel = new int[COLORS_COUNT_IN_RGB];
 
         double[][] h = {{1 / 9, 1 / 9, 1 / 9},
-                {1 / 9, 1 / 9, 1 / 9},
-                {1 / 9, 1 / 9, 1 / 9}};
+                        {1 / 9, 1 / 9, 1 / 9},
+                        {1 / 9, 1 / 9, 1 / 9}};
+
+        int [] newPixel = new int[COLORS_COUNT_IN_RGB];
 
         for (int y = 1; y < height - 1; ++y) {
             for (int x = 1; x < width - 1; ++x) {
-                for (int k = 0; k < COLORS_COUNT_IN_RGB; ++k) {
-                    int pixel0[] = raster.getPixel(x - 1, y - 1, pixel);
-                    int pixel1[] = raster.getPixel(x, y - 1, pixel);
-                    int pixel2[] = raster.getPixel(x + 1, y - 1, pixel);
-                    int pixel3[] = raster.getPixel(x - 1, y, pixel);
-                    int pixel4[] = raster.getPixel(x, y, pixel);
-                    int pixel5[] = raster.getPixel(x + 1, y, pixel);
-                    int pixel6[] = raster.getPixel(x - 1, y + 1, pixel);
-                    int pixel7[] = raster.getPixel(x, y + 1, pixel);
-                    int pixel8[] = raster.getPixel(x + 1, y + 1, pixel);
+                int pixel0[] = raster.getPixel(x - 1, y - 1, pixel);
+                int pixel1[] = raster.getPixel(x, y - 1, pixel);
+                int pixel2[] = raster.getPixel(x + 1, y - 1, pixel);
+                int pixel3[] = raster.getPixel(x - 1, y, pixel);
+                int pixel4[] = raster.getPixel(x, y, pixel);
+                int pixel5[] = raster.getPixel(x + 1, y, pixel);
+                int pixel6[] = raster.getPixel(x - 1, y + 1, pixel);
+                int pixel7[] = raster.getPixel(x, y + 1, pixel);
+                int pixel8[] = raster.getPixel(x + 1, y + 1, pixel);
 
+                for (int k = 0; k < COLORS_COUNT_IN_RGB; ++k) {
                     double colour = (pixel0[k] * h[0][0]) + (pixel1[k] * h[0][1]) + (pixel2[k] * h[0][2]) + (pixel3[k] * h[1][0]) + (pixel4[k] * h[1][1]) + (pixel5[k] * h[1][2]) + (pixel6[k] * h[2][0]) + (pixel7[k] * h[2][1]) + (pixel8[k] * h[2][2]);
 
-                    pixel[k] = (int) colour;
+                    newPixel[k] = (int) colour;
                 }
 
-                raster.setPixel(x, y, pixel);
+                raster.setPixel(x, y, newPixel);
             }
         }
 
