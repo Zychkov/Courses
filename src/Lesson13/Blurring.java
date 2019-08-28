3b2e9da43cbe17138b794a009f581512c0078286
Zychkov <48855208+Zychkov@users.noreply.github.com>
2019-08-28 20:28:59 +0700

    Blurring

--- src/Lesson13/Blurring.java
+++ src/Lesson13/Blurring.java
@@ -19,28 +19,37 @@ public class Blurring {
         int height = raster.getHeight();
 
         final int COLORS_COUNT_IN_RGB = 3;
+        final int MAX_RGB = 255;
 
-        double[][] matrix = {{0.111, 0.111, 0.111},
-                             {0.111, 0.111, 0.111},
-                             {0.111, 0.111, 0.111}};
+        double[][] matrix = {{1.0 / 9, 1.0 / 9, 1.0 / 9},
+                {1.0 / 9, 1.0 / 9, 1.0 / 9},
+                {1.0 / 9, 1.0 / 9, 1.0 / 9}};
 
         int[] pixel = new int[COLORS_COUNT_IN_RGB];
-        int[] newPixel = new int[COLORS_COUNT_IN_RGB];
 
         for (int y = 1; y < height - 1; ++y) {
             for (int x = 1; x < width - 1; ++x) {
+                int[] newPixel = new int[COLORS_COUNT_IN_RGB];
+
                 for (int k = 0; k < COLORS_COUNT_IN_RGB; ++k) {
-                    int blurring = 0;
+                    int colour = 0;
                     int matrixSize = matrix.length / 2;
 
                     for (int i = -matrixSize; i <= matrixSize; i++) {
                         for (int j = -matrixSize; j <= matrixSize; j++) {
                             raster.getPixel(x + i, y + j, pixel);
 
-                            blurring += (int) (pixel[k] * matrix[i + matrixSize][j + matrixSize]);
+                            colour += (int) (pixel[k] * matrix[i + matrixSize][j + matrixSize]);
                         }
                     }
-                    newPixel[k] = blurring;
+
+                    if (colour < 0) {
+                        colour = 0;
+                    } else if (colour > MAX_RGB) {
+                        colour = MAX_RGB;
+                    }
+
+                    newPixel[k] = colour;
                 }
                 raster.setPixel(x, y, newPixel);
             }
